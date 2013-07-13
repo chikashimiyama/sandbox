b46fd44db08caf7a0ff947a4eb031b7fc0694ea3
Chiakshi Miyama <chikashimiyama@me.com>
2013-07-13 11:27:52 +0200

    update

---
+++ stlibtest/mymath.h
@@ -0,0 +1,6 @@
+#ifndef __MYMATH_H
+#define __MYMATH_H
+
+float pythagorean(float x, float y);
+
+#endif
---
+++ stlibtest/test.c
@@ -0,0 +1,7 @@
+#include "mymath.h"
+#include <stdio.h>
+
+int main(void){
+  printf("result:%f\n", pythagorean(3,4));
+  return 0;
+}
