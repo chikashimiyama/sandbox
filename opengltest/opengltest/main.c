fd298867c04d0091762e2df2ef7f3a2b656a63b9
Chkashi Miyama <chikashimiyama@me.com>
2013-10-28 00:46:55 +0100

    upd

--- opengltest/opengltest/main.c
+++ opengltest/opengltest/main.c
@@ -7,12 +7,30 @@
 //
 
 #include <stdio.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/glu.h>
+#include <GLUT/GLUT.h>
 
 int main(int argc, const char * argv[])
 {
+    
+    glutInit(&argc, argv);
+    glutInitWindowPosition(100 , 50);
+	glutInitWindowSize(200 , 100);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    
+	glutCreateWindow("Kitty on your lap");
 
-    // insert code here...
-    printf("Hello, World!\n");
+
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *version = glGetString(GL_VERSION);
+    const GLubyte *glsl_version = glGetString(GL_SHADING_LANGUAGE_VERSION);
+    
+    printf("vendor %s\n", vendor);
+    printf("version %s\n", version);
+    printf("render %s\n", renderer);
+    printf("glsl version %s\n", glsl_version);
     return 0;
 }
 
