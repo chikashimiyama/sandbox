59fb8eb380c3bdf1ffaab66a6436e8ddfa9cf366
Chkashi Miyama <chikashimiyama@me.com>
2013-10-28 00:46:48 +0100

    upd

---
+++ opengltest/opengltest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  opengltest
+//
+//  Created by Chikashi Miyama on 10/19/13.
+//  Copyright (c) 2013 Chikashi Miyama. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
