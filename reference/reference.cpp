f5acb01056bfdc4ce41062e6857fb924dc3520f4
Chikashi Miyama <chikashimiyama@gmail.com>
2014-07-16 00:31:50 +0200

    ref test

---
+++ reference/reference.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+class Test{
+public:
+	int a;
+};
+
+using namespace std;
+
+void changeTest(Test t);
+void changeTestWithRef(Test &t);
+void changeTestWithConstRef(const Test &t);
+void changeTestWithPtr(Test *t);
+void changeTestWithConstPtr(const Test *t);
+
+
+int main(void){
+
+	Test test;
+	test.a = 25;
+
+	changeTest(test); // normal. will be copied
+	cout << test.a	 << endl;
+
+	changeTestWithRef(test); 
+	cout << test.a	 << endl;
+
+	changeTestWithConstRef(test); 
+	cout << test.a	 << endl;
+
+	changeTestWithPtr(&test);
+	cout << test.a	 << endl;
+
+	changeTestWithConstPtr(&test); 
+	cout << test.a	 << endl;
+
+	return 0;
+}
+
+
+void changeTest(Test t){
+	t.a = 125;
+}
+
+void changeTestWithRef(Test &t){
+	t.a = 225;
+}
+
+void changeTestWithConstRef(const Test &t){
+	//t.a = 325; impossible
+}
+
+void changeTestWithPtr(Test *t){
+	t->a = 425;
+}
+
+void changeTestWithConstPtr(const Test *t){
+	//t->a = 525;
+}
